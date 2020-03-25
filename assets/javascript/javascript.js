2a1a10b157eee34ee07f7a4de99c6c8e28869877
Logan Moss <lrdjmoss@gmail.com>
2020-03-25 16:38:01 -0700

    added working AJAX call

---
+++ assets/javascript/javascript.js
@@ -0,0 +1,46 @@
+var api_key = "69AWaR8C4XriE7DTKgm66N7B9jCmhmkN";
+var queryterm = "";
+
+
+var topics = ["Rick and Morty", "The Office", "Brooklyn Nine Nine", "White Collar"];
+
+$(document).ready(loadButtons());
+
+function loadButtons(){
+    $("#buttonDiv").html("");
+    for(let i = 0; i < topics.length; i++){
+        // tempString = topics[i];
+        var tempButton = `<button class=gifSearch name="${topics[i]}">${topics[i]}</button>`;
+        $("#buttonDiv").append(tempButton);
+    }    
+}
+// $("button").click(function(){
+//     if(this != $("#submitButton")){
+//         console.log(this);
+//     }
+// })
+
+$(".gifSearch").click(function(){
+    queryterm = this.name;
+    let queryURL = `https://api.giphy.com/v1/gifs/search?api_key=${api_key}&q=${queryterm}`;
+    // console.log("hello")
+    // console.log(this.name);
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response) {
+        console.log(response);
+    });
+})
+
+
+
+$("#submitButton").click(function(event){
+    event.preventDefault();
+    if($("#gifAddition").val() === ""){
+        console.log("Please enter a valid search");
+    } else {
+        topics.push($("#gifAddition").val());
+    }
+    loadButtons();
+});
